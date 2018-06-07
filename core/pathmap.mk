779c5b56b31b483679efc3f00c040b76c42f7e5f
allianceSparx <alliancesparx@gmail.com>
2018-06-07 22:43:00 +0100

    Alliance-ify

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,6 +93,6 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-ifneq ($(GZOSP_BUILD),)
--include $(TOPDIR)vendor/gzosp/build/core/pathmap.mk
+ifneq ($(ALLIANCE_BUILD),)
+-include $(TOPDIR)vendor/alliance/build/core/pathmap.mk
 endif
--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A GZOSP build needs only the GZOSP product makefiles.
-ifneq ($(GZOSP_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(GZOSP_BUILD)/gzosp.mk")
+# A ALLIANCE build needs only the ALLIANCE product makefiles.
+ifneq ($(ALLIANCE_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(ALLIANCE_BUILD)/alliance.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # GZOSP_BUILD
+endif # ALLIANCE_BUILD
 
-ifeq ($(GZOSP_BUILD),)
+ifeq ($(ALLIANCE_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
